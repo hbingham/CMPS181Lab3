00dddf5e2671221a00c436f75271b5dedc7d4e25
Hunter <hbingham@ucsc.edu>
2018-05-18 18:09:57 -0700

    first commit, merged solution w/ given starting code

---
+++ codebase/ix/ix.cc
@@ -0,0 +1,155 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <cstdio>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+#include "ix.h"
+
+IndexManager* IndexManager::_index_manager = 0;
+
+IndexManager* IndexManager::instance()
+{
+    if(!_index_manager)
+        _index_manager = new IndexManager();
+
+    return _index_manager;
+}
+
+IndexManager::IndexManager()
+{
+    // Initialize the internal PagedFileManager instance
+    _pf_manager = PagedFileManager::instance();
+}
+
+IndexManager::~IndexManager()
+{
+}
+
+RC IndexManager::createFile(const string &fileName)
+{
+    // Creating a new paged file.
+    if (_pf_manager->createFile(fileName))
+        return RBFM_CREATE_FAILED;
+
+    // Setting up the first page.
+    void * firstPageData = calloc(PAGE_SIZE, 1);
+    if (firstPageData == NULL)
+        return RBFM_MALLOC_FAILED;
+    newIndexPage(firstPageData);
+
+    // Adds the first record based page.
+    FileHandle handle;
+    if (_pf_manager->openFile(fileName.c_str(), handle))
+        return RBFM_OPEN_FAILED;
+    if (handle.appendPage(firstPageData))
+        return RBFM_APPEND_FAILED;
+    _pf_manager->closeFile(handle);
+
+    free(firstPageData);
+
+    return SUCCESS;
+
+
+}
+
+RC IndexManager::destroyFile(const string &fileName)
+{
+    fileHandle fd
+    memset(&dev_sys, 0, sizeof dev_sys);
+    return _pf_manager->destroyFile(fileName);
+}
+
+RC IndexManager::openFile(const string &fileName, IXFileHandle &ixfileHandle)
+{
+    return _pf_manager->openFile(fileName.c_str(), ixfileHandle);
+}
+
+RC IndexManager::closeFile(IXFileHandle &ixfileHandle)
+{
+    
+    return _pf_manager->closeFile(ixfileHandle);
+}
+
+RC IndexManager::insertEntry(IXFileHandle &ixfileHandle, const Attribute &attribute, const void *key, const RID &rid)
+{
+    return -1;
+}
+
+RC IndexManager::deleteEntry(IXFileHandle &ixfileHandle, const Attribute &attribute, const void *key, const RID &rid)
+{
+    return -1;
+}
+
+
+RC IndexManager::scan(IXFileHandle &ixfileHandle,
+        const Attribute &attribute,
+        const void      *lowKey,
+        const void      *highKey,
+        bool			lowKeyInclusive,
+        bool        	highKeyInclusive,
+        IX_ScanIterator &ix_ScanIterator)
+{
+    return -1;
+}
+
+void IndexManager::printBtree(IXFileHandle &ixfileHandle, const Attribute &attribute) const {
+}
+
+IX_ScanIterator::IX_ScanIterator()
+{
+}
+
+IX_ScanIterator::~IX_ScanIterator()
+{
+}
+
+RC IX_ScanIterator::getNextEntry(RID &rid, void *key)
+{
+    return -1;
+}
+
+RC IX_ScanIterator::close()
+{
+    return -1;
+}
+
+
+IXFileHandle::IXFileHandle()
+{
+    ixReadPageCounter = 0;
+    ixWritePageCounter = 0;
+    ixAppendPageCounter = 0;
+}
+
+IXFileHandle::~IXFileHandle()
+{
+}
+
+RC IXFileHandle::collectCounterValues(unsigned &readPageCount, unsigned &writePageCount, unsigned &appendPageCount)
+{
+    return -1;
+}
+
+
+//Helper Functions
+// Configures a new record based page, and puts it in "page".
+void IndexManager::newIndexPage(void * page)
+{
+    memset(page, 0, PAGE_SIZE);
+    // Writes the slot directory header.
+    indexDirectoryHeader indexHeader;
+    indexHeader.freeSpaceOffset = PAGE_SIZE;
+    indexHeader.nodeCount = 0;
+    setIndexDirectoryHeader(page, indexHeader);
+}
+
+
+void IndexManager::setIndexDirectoryHeader(void * page, indexDirectoryHeader indexHeader)
+{   
+       // Setting the index directory header.
+    memcpy (page, &indexHeader, sizeof(indexDirectoryHeader));
+}
